40a8600a21e3f8b23bd8f67f9f05ae554997bb76
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-10-07 02:42:04 -0700

    Added navbar with active route. Initializing first three route 'Home', 'Register' and 'LogIn'

--- src/Main Layout/Root.jsx
+++ src/Main Layout/Root.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
+import Route from './Route/Route.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={RouterProvider}/>
+    <RouterProvider router={Route}/>
   </React.StrictMode>,
 )
